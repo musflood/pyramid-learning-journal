c78f413aeb285ef4f32bfe0b689fa6575fd9e68a
Michael Shinners <mishinners@icloud.com>
2017-10-30 16:16:36 -0700

    created basic setup for pyramid work. Installed cookiecutter and init working files.

---
+++ pyramid_learning_journal/views/default.py
@@ -0,0 +1,25 @@
+from pyramid.response import Response
+import os
+
+HERE = os.path.abspath(__file__)
+STATIC = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'static')
+
+
+def list_view(request):
+    """."""
+    pass
+
+
+def detail_view(request):
+    """."""
+    pass
+
+
+def create_view(request):
+    """."""
+    pass
+
+
+def update_view(request):
+    """."""
+    pass
