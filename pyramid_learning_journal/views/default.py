410bd415d55cbd05f6bf1f1ed9717dacf7fef8a8
Michael Shinners <mishinners@icloud.com>
2017-11-01 19:18:02 -0700

    Created detail template.

--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 from pyramid_learning_journal.data import entry_history
-
+from pyramid.exceptions import HTTPNotFound
 
 
 @view_config(route_name='home', renderer='pyramid_learning_journal:templates/list_view.jinja2')
@@ -12,9 +12,17 @@ def list_view(request):
     }
 
 
+@view_config(route_name='detail', renderer='pyramid_learning_journal:templates/detail.jinja2')
 def detail_view(request):
     """A single journal entry."""
-    pass
+    entry_id = int(request.matchdict['id'])
+    if entry_id < 0 or entry_id > len(entry_history.ENTRIES):
+        raise HTTPNotFound
+    entry = list(filter(lambda entry: entry['id'] == entry_id, entry_history.ENTRIES))[0]
+    return {
+        "page_title": "Entry Detail",
+        "entry": entry
+    }
 
 
 def create_view(request):
