7868a43791daad2a5f9018c4fa7729470bc128fb
Michael Shinners <mishinners@icloud.com>
2017-11-02 20:08:17 -0700

    4 unit tests and 2 functional tests written and passing. 100% coverage rate.

--- pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/tests.py
@@ -1,6 +1,6 @@
 """Unit tests basic response test for all view functions."""
 
-
+from pyramid.exceptions import HTTPNotFound
 from pyramid.testing import DummyRequest
 import pytest
 
@@ -43,3 +43,43 @@ def test_update_view_returns_list_of_entries_in_dict(dummy_request):
     response = update_view(dummy_request)
     assert 'entry' in response
     assert isinstance(response['entry'], dict)
+
+
+@pytest.fixture
+def testapp():
+    """Functional test for app."""
+    from webtest import TestApp
+    from pyramid.config import Configurator
+
+    def main():
+        config = Configurator()
+        config.include('pyramid_jinja2')
+        config.include('.routes')
+        config.scan()
+        return config.make_wsgi_app()
+
+    app = main()
+    return TestApp(app)
+
+
+def test_home_route_has_table(testapp):
+    """Test that the home route has a table."""
+    from pyramid_learning_journal.data.entry_history import ENTRIES
+    response = testapp.get("/")
+    assert len(ENTRIES) == len(response.html.find_all('div', 'card'))
+
+
+def test_error_handling_in_detail_view(dummy_request):
+    """Test that the on HTTPNotFound is raised if not found."""
+    from pyramid_learning_journal.views.default import detail_view
+    dummy_request.matchdict['id'] = 99
+    with pytest.raises(HTTPNotFound):
+        detail_view(dummy_request)
+
+
+def test_error_handling_in_update_view(dummy_request):
+    """Test that the on HTTPNotFound is raised if not found."""
+    from pyramid_learning_journal.views.default import update_view
+    dummy_request.matchdict['id'] = 99
+    with pytest.raises(HTTPNotFound):
+        update_view(dummy_request)
